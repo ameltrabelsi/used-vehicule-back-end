84e3cd0e51dd9040914882ed95f58fa9e302c387
ameltrabelsi <ameltrabelsi90@gmail.com>
2024-07-02 17:54:03 +0100

    add check-auth in user-router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const { getUsers, register, login } = require("../controllers/user");
+const checkAuth = require('../middlewares/check-auth');
+const checkIsAdmin = require('../middlewares/checkIsAdmin');
 
 const router = require("express").Router();
 
-router.get("/", getUsers);
-router.post("/", register);
+router.get("/", checkAuth, checkIsAdmin, getUsers);
+router.post("/register", register);
 router.post("/login", login);
 
 module.exports = router;

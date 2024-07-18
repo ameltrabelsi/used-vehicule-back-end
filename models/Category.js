0a8d82e9f4d723b192188086ed68c710e0d06702
ameltrabelsi <ameltrabelsi90@gmail.com>
2024-07-18 15:01:16 +0100

    add usr

--- models/Category.js
+++ models/Category.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 
 const CategorySchema = new mongoose.Schema({
     name:{type: String, required:true, unique: true},
-    description: {type: String}
+    description: {type: String},
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
 })
 
 const Category = mongoose.model("Category", CategorySchema);
